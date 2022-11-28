11a5ba8d8497ec8974f8bd7cff1472c2c212b3f1
MartinaBartolomeo <martinabartolomeo01@gmail.com>
2022-11-28 17:20:30 -0300

    hasta microdesafio 1 M04C18

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.use(express.static("./public"));
+
+app.listen(3000, () =>{
+    console.log("servidor corriendo en http://localhost:3000/")
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+});
+
+app.get('/ayuda', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/ayuda.html"))
+});
+
+app.get('/inicioSesion', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/inicioSesion.html"))
+});
+
+app.get('/inicioSesion', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/misCompras.html"))
+});
+
+app.get('/inicioSesion', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/ofertas.html"))
+});
+
+app.get('/inicioSesion', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/registro.html"))
+});
+
+app.get('/inicioSesion', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/tiendasOficiales.html"))
+});
+
+app.get('/inicioSesion', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/vender.html"))
+});
