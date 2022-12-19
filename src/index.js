869018d04183dec8266a6a242b4ec4d07e98f521
MartinaBartolomeo <martinabartolomeo01@gmail.com>
2022-12-19 20:01:54 -0300

    listo para subir a render

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const path = require('path');
 
 app.use(express.static("./public"));
 
-app.listen(3030, () =>{
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log("servidor corriendo en el puerto ${port}"));
+
+/* app.listen(3030, () =>{
     console.log("servidor corriendo en http://localhost:3030/")
-});
+}); */
 
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
